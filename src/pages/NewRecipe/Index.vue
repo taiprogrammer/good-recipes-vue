c544fc43e21936af57f349895a404354770a49b8
taiprogrammer <taiza.marques@sptech.school>
2023-08-23 18:04:33 -0300

    feature (App) #10 adding sidebar component

--- src/pages/NewRecipe/Index.vue
+++ src/pages/NewRecipe/Index.vue
@@ -1,64 +1,67 @@
 <template>
     <Header />
     <main>
-        <div class="container">
-            <h1>Envie sua receita</h1>
-
-            <div class="container-recipe">
-                <div class="wrapper">
-                    <div class="side-one">
-                        <div class="wrapper_upload">
-                            <img src="../../assets/no-image/cover.png" alt="">
-                            <label class="label_file_upload" for="file_upload">
-                                <PhPencil :size="30" />
-                            </label>
-                        </div>
-
-                        <input type="file" id="file_upload">
+        <Sidebar />
+        <div class="info">
+            <div class="title">
+                <PhNotepad size="25" />
+                <h1>Enviar receita</h1>
+            </div>
+            <div class="wrapper">
+                <div class="side-one">
+                    <div class="wrapper_upload">
+                        <img src="../../assets/no-image/cover.png" alt="">
+                        <label class="label_file_upload" for="file_upload">
+                            <PhPencil :size="30" />
+                        </label>
                     </div>
-                    <div>
-                        <form>
-                            <div class="field">
-                                <label for="titulo">Título da Receita</label>
-                                <input type="text" name="titulo" id="titulo">
-                            </div>
-                            <div class="time">
-                                <label>Tempo de preparo</label>
-                                <div class="time-container">
-                                    <div class="field">
-                                        <label for="hora">Hora(s)</label>
-                                        <input type="number" name="hora" id="hora">
-                                    </div>
-                                    <div class="field">
-                                        <label for="minuto">Minuto(s)</label>
-                                        <input type="number" name="minuto" id="minuto">
-                                    </div>
-                                    <div class="field">
-                                        <label for="segundo">Segundo(s)</label>
-                                        <input type="number" name="segundo" id="segundo">
-                                    </div>
+                    <input type="file" id="file_upload">
+                </div>
+                <div class="side-two">
+                    <form>
+                        <div class="field">
+                            <label for="titulo">Título da Receita</label>
+                            <input type="text" name="titulo" id="titulo">
+                        </div>
+                        <div class="time">
+                            <label>Tempo de preparo</label>
+                            <div class="time-container">
+                                <div class="field">
+                                    <label for="hora">Hora(s)</label>
+                                    <input type="number" name="hora" id="hora">
+                                </div>
+                                <div class="field">
+                                    <label for="minuto">Minuto(s)</label>
+                                    <input type="number" name="minuto" id="minuto">
+                                </div>
+                                <div class="field">
+                                    <label for="segundo">Segundo(s)</label>
+                                    <input type="number" name="segundo" id="segundo">
                                 </div>
                             </div>
-                            <div class="field">
-                                <label for="porcoes">Número de porções</label>
-                                <input type="number" name="porcoes" id="porcoes">
-                            </div>
-                            <div class="field">
-                                <label for="ingredientes" class="ingredientes">Ingredientes</label>
-                                <QuillEditor v-model:content="ingredientes" theme="snow" toolbar="minimal" placeholder="Digite sua receita aqui">
-                                </QuillEditor>
-                            </div>
-                            <div class="field">
-                                <label for="preparo" class="preparo">Modo de preparo</label>
-                                <QuillEditor v-model:content="modoPreparo" theme="snow" toolbar="minimal" placeholder="Digite o modo de preparo aqui">
-                                </QuillEditor>
-                            </div>
-                            <button>Enviar receita</button>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="field">
+                            <label for="porcoes">Número de porções</label>
+                            <input type="number" name="porcoes" id="porcoes">
+                        </div>
+                        <div class="field">
+                            <label for="ingredientes" class="ingredientes">Ingredientes</label>
+                            <QuillEditor v-model:content="ingredientes" theme="snow" toolbar="minimal"
+                                placeholder="Digite sua receita aqui">
+                            </QuillEditor>
+                        </div>
+                        <div class="field">
+                            <label for="preparo" class="preparo">Modo de preparo</label>
+                            <QuillEditor v-model:content="modoPreparo" theme="snow" toolbar="minimal"
+                                placeholder="Digite o modo de preparo aqui">
+                            </QuillEditor>
+                        </div>
+                        <button>Enviar receita</button>
+                    </form>
                 </div>
             </div>
         </div>
+
     </main>
     <Footer />
 </template>
@@ -67,9 +70,10 @@
 import Header from '../../components/Header.vue';
 import Footer from '../../components/Footer.vue';
 
-import { PhPencil } from '@phosphor-icons/vue';
+import { PhPencil, PhNotepad } from '@phosphor-icons/vue';
 import { QuillEditor } from '@vueup/vue-quill';
 import { ref } from 'vue';
+import Sidebar from '../../components/Sidebar.vue';
 
 const ingredientes = ref("");
 const modoPreparo = ref("")
@@ -78,26 +82,35 @@ const modoPreparo = ref("")
 <style lang="css" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
 
-.container {
+main {
     width: 90%;
-    margin: 0 auto;
+    margin: 3rem auto;
+
+    gap: 4rem;
+    display: flex;
 }
 
-h1 {
-    font-size: 3.5rem;
-    font-family: 'Anton', sans-serif;
+.info {
+    width: 100%;
+}
 
-    color: var(--green250);
-
-    margin: 2rem 0;
+.title {
+    gap: 0.5rem;
+    display: flex;
+    align-items: center;
 }
 
 .wrapper {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
+    width: 100%;
+    max-height: 540px;
+    overflow-y: auto;
 
-    margin: 1rem 0;
+    padding: 1rem;
+}
+
+.side-one {
+    width: fit-content;
+    margin: 3rem 0;
 }
 
 img {
@@ -113,10 +126,7 @@ input[type="file"] {
     cursor: pointer;
     width: fit-content;
 }
-.side-one {
-    align-items: center;
-    margin: auto 0;
-}
+
 .wrapper_upload {
     position: relative;
     width: fit-content;
@@ -155,9 +165,9 @@ label {
 }
 
 .time-container {
-    display: flex;
+    gap: 5rem;
     width: 97%;
-    justify-content: space-between;
+    display: flex;
 
     margin-top: 0.5rem;
 }
@@ -185,15 +195,16 @@ input {
 }
 
 button {
-    border: 0;
-    border-radius: 0.2rem;
+    color: var(--gray090);
+    background: var(--green750);
 
-    cursor: pointer;
+    border: 0;
+    padding: 1rem 1.5rem;
+    border-radius: 0.5rem;
 
     margin-top: 1rem;
-    color: var(--gray090);
-    padding: 0.6rem 3rem;
-    background: var(--green750);
+
+    cursor: pointer;
 
     transition: all 0.3s ease-in-out;
 }
@@ -202,7 +213,8 @@ button:hover {
     opacity: 0.8;
 }
 
-.ingredientes, .preparo {
+.ingredientes,
+.preparo {
     margin-bottom: 0.5rem;
 }
 </style>
