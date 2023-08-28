0da8db0b8bc45631b341e1d2b8de144329bda5d5
taiprogrammer <taiza.marques@sptech.school>
2023-08-28 12:17:50 -0300

    feature (Home) #25 - adding most recent recipes calling endpoint from API

--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -5,12 +5,23 @@
     <main>
         <div class="divider"></div>
         <h1>Últimos adicionados</h1>
-        <div class="container-recipes">
-            <RecipeCard :imagem-url="null" nome="Receita 1" tempo="1:20:30" :porcoes="2" />
+        <div class="container-loader" v-if="isLoadingRecents">
+            <Loader />
+        </div>
+        <div class="container-recipes" v-else-if="mostRecentsRecipes !== null">
+            <div v-for="(recipe, key) in mostRecentsRecipes">
+                <RecipeCard 
+                :imagem-url="recipe.imagem" 
+                :nome="recipe.nome" 
+                :tempo="`${recipe.horas}:${recipe.minutos == 0 ? '00' : recipe.minutos}:${recipe.segundos == 0 ? '00' : recipe.segundos}`"
+                :porcoes="recipe.porcoes"
+                :key="`${key}_${recipe.receitaId}`"/>
+            </div>
+            <!-- <RecipeCard :imagem-url="null" nome="Receita 1" tempo="1:20:30" :porcoes="2" />
             <RecipeCard :imagem-url="null" nome="Receita 2" tempo="1:20:30" :porcoes="4" />
             <RecipeCard :imagem-url="null" nome="Receita 3" tempo="1:20:30" :porcoes="6" />
             <RecipeCard :imagem-url="null" nome="Receita 4" tempo="1:20:30" :porcoes="8" />
-            <RecipeCard :imagem-url="null" nome="Receita 5" tempo="1:20:30" :porcoes="10" />
+            <RecipeCard :imagem-url="null" nome="Receita 5" tempo="1:20:30" :porcoes="10" /> -->
         </div>
         <div class="divider"></div>
         <h1>Os mais acessados</h1>
@@ -26,11 +37,36 @@
 </template>
 
 <script setup>
-import Header from '../../components/Header.vue';
 import Topic from '../../components/Topic.vue';
+import Footer from '../../components/Footer.vue';
+import Header from '../../components/Header.vue';
+import Loader from '../../components/Loader.vue';
 import Carousel from '../../components/Carousel.vue';
 import RecipeCard from '../../components/RecipeCard.vue';
-import Footer from '../../components/Footer.vue';
+
+import { ref, onMounted } from 'vue';
+import { api } from '../../services/index';
+
+const isLoadingRecents = ref(true);
+const mostRecentsRecipes = ref(null);
+
+async function getMostRecentsRecipes() {
+    api.get('/recipe/recents').then(async ({ data }) => {
+        if (data.length > 0) {
+            mostRecentsRecipes.value = data;
+
+            setTimeout(() => {
+                isLoadingRecents.value = false;
+            }, 2500)
+        }
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+onMounted(async () => {
+    await getMostRecentsRecipes();
+})
 </script>
 
 <style lang="css" scoped>
@@ -51,6 +87,11 @@ import Footer from '../../components/Footer.vue';
     grid-template-columns: repeat(5, 1fr);
 
     margin: 1rem 0;
+}
+
+.container-loader {
+    width: 100%;
+    margin: 0 auto;
 }
 
 main {
